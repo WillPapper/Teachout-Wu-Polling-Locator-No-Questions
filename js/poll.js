7a1d782fc1435abcc63d11ed926e661c03d6ed12
William Papper <contact@papper.me>
2014-09-07 12:34:22 -0500

    Fixed div ID

--- js/poll.js
+++ js/poll.js
@@ -32,7 +32,7 @@ function checkAuth() {
 function GetResponse(resp,rawResp) {
   // clear any old fields
   document.getElementById("PollingPlace").style.display="block";
-  document.getElementById("PollingText").style.display="block";
+  document.getElementById("PollText").style.display="block";
   document.getElementById("ppName").innerText="";
   document.getElementById("ppLine1").innerText="";
   document.getElementById("ppLine2").innerText="";
